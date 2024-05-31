1427a144d545a05859916cfafe0e7ed74aa7dfd8
franklin-raph <igboekwulusifranklin@gmail.com>
2024-06-01 00:43:56 +0100

    update

--- app/components/leader-board-component/LeaderBoardComponent.tsx
+++ app/components/leader-board-component/LeaderBoardComponent.tsx
@@ -13,15 +13,33 @@ const LeaderBoardComponent = () => {
                     <div className='bg-primary-color w-full h-[150px] rounded-[11px] text-white p-3'>
                         <p className='text-[24px] font-[500]'>Al-Time <br /> Leaderboard</p>
                     </div>
-                    <div className='bg-[#899EFD1A] inline-flex items-center gap-5 mt-7 rounded-md'>
+                    <div className='bg-[#899EFD1A] inline-flex items-center mt-7 rounded-md'>
                         <div className='flex items-center gap-3 border-r border-gray-300 py-2 px-4'>
                             <img src="./images/tag-user.svg" alt="" />
-                            <p className='text-primary-color'>139181 Users</p>
+                            <p className='text-primary-color text-[10px]'>139181 Users</p>
                         </div>
-                        <div className='flex items-center gap-3  py-2 px-4'>
+                        <div className='flex items-center gap-3 py-2 px-4'>
                             <img src="./images/cup-colored.svg" alt="" />
-                            <p className='text-primary-color'>20 Tiers</p>
+                            <p className='text-primary-color text-[10px]'>20 Tiers</p>
                         </div>
+                    </div>
+                    <div className='mt-10 mx-[30px]'>
+                        {
+                            Array.from({ length: 10 }).map((item, index) => {
+                                return (
+                                    <div className='flex items-center justify-between my-12'>
+                                        <div className='flex items-center gap-7'>
+                                            <p>{index + 1}.</p>
+                                            <div className='flex items-center gap-3'>
+                                                <img src="./images/user.svg" alt="" />
+                                                <p>GameChanger</p>
+                                            </div>
+                                        </div>
+                                        <p>6465</p>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </div>
--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import DashboardComponent from '../components/dashboard-component/DashboardComponent'
+import OverViewComponent from '../components/overview-component/OverViewComponent'
 
 const page = () => {
   return (
     <div>
-        <DashboardComponent />
+        <OverViewComponent />
     </div>
   )
 }
